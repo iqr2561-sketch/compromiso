a96b083abfdf6c3f364e36981777e490c07a0dd1
mjsis <mjsis@github.com>
2026-01-11 20:07:36 -0300

    Optimize DB connection: Remove global SSL disable, rely on Pool config

--- api/lib/db.js
+++ api/lib/db.js
@@ -2,9 +2,6 @@ import pg from 'pg';
 
 const { Pool } = pg;
 
-// Permitir certificados autofirmados en desarrollo para evitar errores de conexión
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
-
 // Configuración optimizada para Supabase
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
